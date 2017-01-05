0026d6b8167440b9da576cbccb117a6d1e758c8e
noah lopez <thenman534@gmail.com>
2017-01-05 21:11:08 +0000

    added first project

---
+++ traffic.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    //Write all your jQuery code here
+        $("button").click(function() {
+      $("#like").html("You like this project!");
+    });
+    
+    // ALL JAVASCRIPT BELOW HERE
+    
+$("#colorred").dblclick(function() {
+    $("#box").css("background-color", "#ff0000");
+});
+
+$("#colorgreen").dblclick(function() {
+    $("#yea").css("background-color", "#33ff33");
+});
+
+$("#coloryellow").dblclick(function() {
+    $("#sure").css("background-color","#ffff66 ");
+});
+    
+    
+    // ALL JAVASCRIPT ABOVE HERE
+
+});
+
+
+
+
+
